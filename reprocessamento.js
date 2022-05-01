bdefae2320727d7810bddb9e659c74cbae1cbae4
Diego Borgheti <borgheti.d@gmail.com>
2022-04-30 22:19:11 -0300

    ton

---
+++ reprocessamento.js
@@ -0,0 +1,133 @@
+    var userIds = [
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        'e545e73e-87bb-42ab-9db0-d95b023b0e6d',
+        '7464f7f4-0253-41cc-aa0c-3ec1112f6875',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        '284be02f-cb0b-4895-ad81-1800f0c181c6',
+        'e3306697-92eb-4141-bbce-1440bac6d93e',
+        'f9dedb65-7350-40f8-8e89-08714f7371be',
+        'f9dedb65-7350-40f8-8e89-08714f7371be',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        'e545e73e-87bb-42ab-9db0-d95b023b0e6d',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        'e3306697-92eb-4141-bbce-1440bac6d93e',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        '7464f7f4-0253-41cc-aa0c-3ec1112f6875',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        '7464f7f4-0253-41cc-aa0c-3ec1112f6875',
+        'f9dedb65-7350-40f8-8e89-08714f7371be',
+        'e3306697-92eb-4141-bbce-1440bac6d93e',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        'e3306697-92eb-4141-bbce-1440bac6d93e',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        'f9dedb65-7350-40f8-8e89-08714f7371be',
+        '7464f7f4-0253-41cc-aa0c-3ec1112f6875',
+        'e3306697-92eb-4141-bbce-1440bac6d93e',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        '7464f7f4-0253-41cc-aa0c-3ec1112f6875',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '284be02f-cb0b-4895-ad81-1800f0c181c6',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        '81e9f04e-5a64-43b1-b87c-ecd02efbdc98',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        'e545e73e-87bb-42ab-9db0-d95b023b0e6d',
+        '284be02f-cb0b-4895-ad81-1800f0c181c6',
+        'e545e73e-87bb-42ab-9db0-d95b023b0e6d',
+        '7464f7f4-0253-41cc-aa0c-3ec1112f6875',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        'f9dedb65-7350-40f8-8e89-08714f7371be',
+        '61f7d1aa-47cd-4ff7-963a-9310553adba0',
+        'aca54f57-b04b-4739-9f56-8420d5657fb0',
+        'e3306697-92eb-4141-bbce-1440bac6d93e',
+        '284be02f-cb0b-4895-ad81-1800f0c181c6'
+    ];
+
+
+    var userEmails = [
+        'alinecon@gmail.com',
+        'cris2922@gmail.com',
+        'inamker@gmail.com',
+        'sophieadm@tokiomarine.com.br',
+        'alinecon@gmail.com',
+        'caroline@bol.com.br',
+        'benedito.eduardoc@hotmail.com',
+        'teresinharayssa@gmail.com',
+        'eliane.bruna@gmail.com',
+        'eliane.bruna@gmail.com',
+        'cris2922@gmail.com',
+        'inamker@gmail.com',
+        'cris2922@gmail.com',
+        'teresinharayssa@gmail.com',
+        'cris2922@gmail.com',
+        'sophieadm@tokiomarine.com.br',
+        'caroline@bol.com.br',
+        'sophieadm@tokiomarine.com.br',
+        'eliane.bruna@gmail.com',
+        'teresinharayssa@gmail.com',
+        'alinecon@gmail.com',
+        'alinecon@gmail.com',
+        'teresinharayssa@gmail.com',
+        'alinecon@gmail.com',
+        'cris2922@gmail.com',
+        'eliane.bruna@gmail.com',
+        'sophieadm@tokiomarine.com.br',
+        'teresinharayssa@gmail.com',
+        'cris2922@gmail.com',
+        'alinecon@gmail.com',
+        'caroline@bol.com.br',
+        'sophieadm@tokiomarine.com.br',
+        'alinecon@gmail.com',
+        'alinecon@gmail.com',
+        'benedito.eduardoc@hotmail.com',
+        'cris2922@gmail.com',
+        'alinecon@gmail.com',
+        'caroline@bol.com.br',
+        'caroline@bol.com.br',
+        'cris2922@gmail.com',
+        'alinecon@gmail.com',
+        'caroline@bol.com.br',
+        'caroline@bol.com.br',
+        'inamker@gmail.com',
+        'benedito.eduardoc@hotmail.com',
+        'inamker@gmail.com',
+        'sophieadm@tokiomarine.com.br',
+        'cris2922@gmail.com',
+        'eliane.bruna@gmail.com',
+        'caroline@bol.com.br',
+        'cris2922@gmail.com',
+        'teresinharayssa@gmail.com',
+        'benedito.eduardoc@hotmail.com'
+    ];
+
+const fixTransactions = (userIds) => {
+    console.log(userIds)
+    console.log('Corrigindo transações dos usuários enviados')
+  }
+  
+  const sendEmails = (userEmails) => {
+      console.log(userEmails)
+    console.log('Enviando comunicação para os usuários impactados')
+  }
+  
+  const main = async () => {
+    await fixTransactions(userIds)
+    await sendEmails(userEmails)
+  }
+  
+  main()
+    .then(() => console.log('Transações reprocessadas!')
+    .catch(err => console.error(err))
+
+
